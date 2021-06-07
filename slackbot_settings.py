4c9d03d91a8129f7bc7d287958dcab270d0060e9
tama-shira <i.warn.go.fall.down@gmail.com>
2021-06-08 00:57:06 +0900

    bot test

---
+++ slackbot_settings.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# botアカウントのトークンを指定
+API_TOKEN = "xoxb-454244541382-2168299438480-t2jK9IkFtZvjjL8ysc9DaGaf"
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "何言ってんだこいつ"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
